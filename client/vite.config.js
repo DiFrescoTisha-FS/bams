d1732bbec29fe93848e91306759568ea9cb01acd
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2024-05-29 15:16:45 -0400

    chore: Update Vite Configuration file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-      host: '0.0.0.0',
-      port: parseInt(process.env.VITE_PORT, 10) || 5173,
-      proxy: {
-          '/auth': {
-              target: 'http://localhost:4008',
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/auth/, '/auth'),
-          },
-          '/api': {
-              target: 'http://localhost:4008',
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api/, '/api'),
-          },
-      },
-  },
-  build: {
-      outDir: 'dist',
-  },
-});
+})
